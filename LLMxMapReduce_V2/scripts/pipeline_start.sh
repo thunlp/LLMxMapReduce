37bc47742ab568d626835cb137947850f70bd155
R0k1e <wanghaoyu666@bupt.edu.cn>
2025-04-09 13:58:50 +0800

    update .gitignore

--- LLMxMapReduce_V2/scripts/pipeline_start.sh
+++ LLMxMapReduce_V2/scripts/pipeline_start.sh
@@ -7,10 +7,9 @@ log_dir="./output/log"
 mkdir -p $log_dir
 
 log_file="${log_dir}/${timestamp}_$(basename "$output_file_path").log"
-  # --topic "Multi Modal LLM" \
 python ./src/start_pipeline.py \
-  --input_file "output/recp_3_layer_6_bestof_3_refinecount_3_20_20250211_001804_block_1.jsonl" \
-  --output_file output/fig_data.json \
+  --topic ${topic} \
+  --output_file ${output_file_path} \
   --config_file ./config/model_config.json\
   --block_count 0\
   --data_num 1\
