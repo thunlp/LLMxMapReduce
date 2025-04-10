ee594985374bc476dfedb70e6e57a941e7422c19
R0k1e <wanghaoyu666@bupt.edu.cn>
2025-04-10 23:33:02 +0800

    update data count

--- LLMxMapReduce_V2/src/encode/encode_pipeline.py
+++ LLMxMapReduce_V2/src/encode/encode_pipeline.py
@@ -4,6 +4,7 @@ import random
 from async_d import Node
 from async_d import Sequential
 from gevent.fileobject import FileObject
+from threading import Lock
 
 from src.data_structure.survey import Survey
 import logging
@@ -11,10 +12,12 @@ logger = logging.getLogger(__name__)
 
 
 class EncodePipeline(Sequential):
-    def __init__(self, configs, data_num, output_file, worker_num=1):
+    def __init__(self, configs, data_num, worker_num=1):
         self.configs = configs
         self.worker_num = worker_num
         self.data_num = data_num
+        self.processed_count = 0
+        self._count_lock = Lock()
 
         self.load_node = Node(
             self.load_survey, worker_num=self.worker_num
@@ -35,16 +38,23 @@ class EncodePipeline(Sequential):
     def load_survey(self, input_file):
         with FileObject(input_file, "r") as f:
             for line in f:
-                if (self.data_num != None and self.data_num <= 0):
-                    break
+                with self._count_lock:
+                    if self.data_num is not None:
+                        if self.processed_count >= self.data_num:
+                            break
+                        self.processed_count += 1
+                
                 survey = Survey(json.loads(line))
-                if self.data_num != None:
-                    self.data_num -= 1
                 yield survey
             else:
                 logger.info("All data in input file has been loaded.")
-        if self.data_num != None and self.data_num > 0:
-            logger.warning(f"Only {self.data_num} data in input file, less than specified data_num.")
+        
+        with self._count_lock:
+            if self.data_num is not None and self.processed_count < self.data_num:
+                logger.warning(
+                    f"Only {self.processed_count} data in input file, "
+                    f"less than specified data_num {self.data_num}."
+                )
         return
 
     def unpack(self, survey):
